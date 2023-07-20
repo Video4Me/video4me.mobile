c0b2cc83174b32f080d9a805cd952ffb4aa1d83f
Alec Lima <aleclimadev@gmail.com>
2023-07-20 16:36:56 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>Video4Me</Text>
+    </View>
+  );
+};
+export default App;
