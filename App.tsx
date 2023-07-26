380dc1b20deea97b34c96516f5bc4606376c50ad
Alec Lima <aleclimadev@gmail.com>
2023-07-26 17:53:57 -0300

    feat: emptystate

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import Register from './src/screens/register';
+import EmptyState from './src/modules/EmptyState/screens';
 
 const App = () => {
   return (
     <View>
+      <EmptyState />
       <Register />
     </View>
   );
